f8aa2618a8ce9a9fca49e6fb22dece07aa196a60
uriel71 <124410672+uriel71@users.noreply.github.com>
2023-11-14 18:35:17 -0700

    Add files via upload

---
+++ parcial3/AsignarMaterias.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Asignar materias</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="code.jquery.com_jquery-3.7.1.js"></script>
+</head>
+<body>
+    <?php
+        include 'menu.php';
+        include 'conexion.php';
+        $sql_alumnos ="SELECT * FROM alumnos";
+        $sql_materias = "SELECT *FROM materias";
+        $datos_alumnos = $conexion->query($sql_alumnos);
+        $datos_materias = $conexion->query($sql_materias);
+    ?>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 card p-4 m-2">
+                <h1>Asignar materias a alumno</h1>
+                <form action="">
+                    <select name="alumno" class="form-control">
+                    <?php if( $datos_alumnos->num_rows>0){
+                        while($registro = $datos_alumnos->fetch_assoc()){ ?>
+                            <option value="<?php echo $registro["id"]; ?>"><?php echo $registro["nombre"]; ?></option> 
+                        <?php } ?>
+                    <?php } ?>
+
+                    </select>
+                    <h3>seleccione las materias</h3>
+                    <?php if( $datos_alumnos->num_rows>0){
+                         while($registro = $datos_materias->fetch_assoc()){ ?>
+                         <p><input type="checkbox" name="materias[]" value=""> <?php echo $registro["nombre"];?></p>
+                         <?php } } ?>
+                         
+                         <div>
+                            <input type="submit" class="btn btn-primary" value="Asignar">
+                            <a href="" class="btn btn-danger">Cancelar</a>
+                         </div>
+                        
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <footer class="text-center">
+        <hr>
+        Cetis 107 &copy; 2023
+    </footer>
+
+    <script src="js/bootstrap.bundle.js"></script>  
+
+</body>
+</html>
--- parcial3/editarAlumno.php
+++ parcial3/editarAlumno.php
@@ -2,64 +2,60 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrar Alumno</title>
-    <link rel="stylesheet" href="css/bootstrap.css"> 
+    <title>Registrar alumno</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
     <script src="code.jquery.com_jquery-3.7.1.js"></script>
 </head>
 <body>
     <?php 
         include 'menu.php'; 
         include 'conexion.php';
-
         $id = $_GET["id"];
-
-        $sql = "SELECT * FROM alumnos WHERE id=".$id;
+        $sql = "SELECT  * FROM alumnos WHERE id=".$id;
 
         $datos = $conexion->query($sql);
-        
         $alumno = $datos->fetch_assoc();
+    ?> 
 
-    ?>
     <div class="container">
         <div class="row">
+            <h2>Registrar Alumno</h2>
             <div class="col-12 card m-4 p-4">
-                <h2>Registrar Alumno</h2><hr>
-                <form action="ActualizarAlumno.php" method="POST">
+                <form action="actualizarRegistro.php" method="POST">
                     <input type="hidden" name="id" value="<?php echo $alumno["id"]; ?>">
                     <div class="form-group">
                         <label for="">Nombre:</label>
-                        <input value="<?php echo $alumno["nombre"]; ?>" name="nombre" type="text" class="form-control" placeholder="Teclea el nombre del alumno" required>
+                        <input type="text" name="nombre" value="<?php echo $alumno["nombre"]; ?>" class="form-control" placeholder="Teclea tu nombre" required>
                     </div>
                     <div class="form-group">
                         <label for="">Número de control:</label>
-                        <input value="<?php echo $alumno["numero_control"]; ?>" name="nc" type="text" class="form-control" placeholder="Teclea el número de control" required>
+                        <input type="text" name="nc" value="<?php echo $alumno["numero_control"]; ?>" class="form-control" placeholder="Teclea tu número de control" required>
                     </div>
                     <div class="form-group">
                         <label for="">Semestre:</label>
-                        <input value="<?php echo $alumno["semestre"]; ?>" name="semestre" type="number" class="form-control" placeholder="Teclea el semestre" required>
+                        <input type="number" name="semestre" value="<?php echo $alumno["semestre"]; ?>" class="form-control" placeholder="Teclea tu semestre" required>
                     </div>
                     <div class="form-group">
                         <label for="">Edad:</label>
-                        <input value="<?php echo $alumno["edad"]; ?>" name="edad" type="number" class="form-control" placeholder="Teclea la edad" required>
+                        <input type="number" name="edad" value="<?php echo $alumno["edad"]; ?>" class="form-control" placeholder="Teclea tu edad" required>
                     </div>
                     <div class="form-group">
                         <label for="">Turno:</label>
                         <select name="turno" class="form-control" required>
-                            <option selected value="<?php echo $alumno["turno"]; ?>"><?php echo $alumno["turno"]; ?></option>
+                            <option value="<?php echo $alumno["turno"]; ?>"><?php echo $alumno["turno"]; ?></option>
                             <option value="">Selecciona el turno</option>
-                            <option value="MATUTINO">MATUTINO</option>
-                            <option value="VESPERTINO">VESPERTINO</option>
+                            <option value="Matutino">Matutino</option>
+                            <option value="Vespertino">Vespertino</option>
                         </select>
                     </div>
                     <div class="form-group">
                         <label for="">Sexo:</label>
                         <select name="sexo" class="form-control" required>
+                            <option value="<?php echo $alumno["sexo"]; ?>"><?php echo $alumno["sexo"]; ?></option>
                             <option value="">Selecciona el sexo</option>
-                            <option selected value="<?php echo $alumno["sexo"]; ?>"><?php echo $alumno["sexo"]; ?></option>
-                            <option value="0">FEMENINO</option>
-                            <option value="1">MASCULINO</option>
+                            <option value="0">Femenino</option>
+                            <option value="1">Masculino</option>
                             <option value="2">Prefiero no responder</option>
                         </select>
                     </div><br>
@@ -72,11 +68,6 @@
         </div>
     </div>
 
-    <footer class="text-center">
-        <hr>
-        CETIS 107 &copy; 2023
-    </footer>
-
-    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>  
 </body>
 </html>
